b36383570028980b765a2295754d37869328ff94
Patricia <patricia.corduant@gmail.com>
2021-06-01 07:10:19 +0200

    versionning and index.js hashrouterdelete

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-      <HashRouter>
+   
     <App />
-    </HashRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -16,4 +16,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals();
