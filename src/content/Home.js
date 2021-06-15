dc4a04a2b598a43798a077266fe199c4ef8f127d
Patricia <patricia.corduant@gmail.com>
2021-06-15 16:44:23 +0200

    colors

--- src/content/Home.js
+++ src/content/Home.js
@@ -3,18 +3,25 @@ import ReactTypingEffect from 'react-typing-effect';
 import profilepic from '../img/medailloncv.png';
 
 
-
 class Home extends Component {
     render() {
         return (
             <div className="condiv-home">
-                <img src={profilepic} id = "profil" className="profilepic" alt="me" width="300" height="300" ></img>
+
                 <ReactTypingEffect className="typingeffect" text={['I am Corduant Patricia', 'I am a web developer']} speed={200} eraseDelay={500} />
-            
+
+                <div>
+                    <img src={profilepic} id="profil" className="profilepic" alt="me" width="300" height="300" ></img>
+                </div>
+                          
 
             </div>
+            
 
         )
+        
     }
 }
 export default Home
+
+
